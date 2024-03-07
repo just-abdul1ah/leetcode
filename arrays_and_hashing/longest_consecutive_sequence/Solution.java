6012222b86788b99edfb70a0a72bbc8010488eb8
Abdullojon Yusupov <email.abdullojon@gmial.com>
2024-03-07 21:28:51 +0900

    longest_consequtive_sequence solved

--- arrays_and_hashing/longest_consecutive_sequence/Solution.java
+++ arrays_and_hashing/longest_consecutive_sequence/Solution.java
@@ -1,27 +1,19 @@
-import java.util.Map;
-import java.util.HashMap;
+import java.util.*;
 
 public class Solution {
     public int longestConsecutive(int[] nums) {
         if (nums.length < 2) return nums.length;
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(nums[0], 1);
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums)
+            set.add(n);
         int answ = 1;
-        for (int i = 1; i < nums.length; i++) {
-            int n = nums[i];
-            if (map.containsKey(n)) continue;
-            int l = map.getOrDefault(n - 1, 0);
-            int r = map.getOrDefault(n + 1, 0);
-            int s = l + r + 1;
-            if (l + r == 0) {
-                map.put(n, s);
-            } else {
-                map.put(n - l, s);
-                map.put(n + r, s);
-                if (l != 0 && r != 0) map.put(n, s);
+        for (int n : set) {
+            if (!set.contains(n - 1)) {
+                int c = 1; n++;
+                while (set.contains(n++))
+                    c++;
+                if (c > answ) answ = c;
             }
-            
-            if (s > answ) answ = s;
         }
         return answ;
     }
